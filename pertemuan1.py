252bfe9e7fa152069f16686ed1bea4454841af91
unknown <dewanarendra115@gmail.com>
2021-12-05 15:19:11 +0700

    menambah file pertemuan2 dan menghapus file tidak perlu

---
+++ pertemuan1.py
@@ -0,0 +1,37 @@
+#ini comment
+
+#ini nama
+print("Halo,Dunia")
+#ini alamat saya
+print("rumah saya di Kalsel")
+
+#variabel
+x =3 #integer
+y ="Hello, World" #string
+z = 2.5 #float
+
+print(x)
+print(y) 
+print(z)
+
+#type data
+print(type(x))
+print(type(y))
+print(type(z))
+
+#math operations
+p = 2
+q = 3
+r = p + q
+
+print(p**q) #exponential
+print(r)
+
+#casting
+x = int(2.5)
+y = str(-3.7)
+z = float("29")
+
+print(x)
+print(y)
+print(z)
---
+++ pertemuan2.py
@@ -0,0 +1,102 @@
+#user input
+
+nama = input("masukan nama anda: ") # default string
+umur = int(input("masukan umur: "))
+
+print("nama saya adalah:" + nama)
+print("umur:" + umur)
+
+#menghitung luas persegi panjang
+p = int(input("masukan panjang:" ))
+l = int(input("masukan lebar: "))
+L = p*l 
+k = 2*p + 2*l 
+print("maka luas persegi panjang : {} cm^2 dan kelilingnya : {} cm".format(L,k))
+
+x = "Indonesia"
+y = "AI"
+print(x + " " + y)
+
+#string operations
+a = "Halo, Dunia!"
+print(a[0]) #karakter ke-0 adalah huruf H
+print(a[2:8]) #mulai dari karakter ke-2 sd ke-7
+print(len(a)) #menghitung panjang karakter
+print(a[0] + " " + a[2]) #menampilkan huruf H dan l
+
+#Quiz
+x = "Indonesia AI"
+print(x[2:4]) #karakter ke 2 sd 3
+print(len(x))
+
+#boolean
+print(8 < 9)
+print(8 > 9)
+
+#condition
+a = 200
+b = 200
+if b < a:
+    print("a is greater than b")
+elif a == b:
+    print("a and b are equal")
+else:
+    print("b is greater than a")
+
+nilai = int(input("masukan nilai anda: "))
+kkm = 70
+
+if nilai < kkm:
+    print("anda harus mengulang")
+else:
+    print("selamat anda lulus")
+    print("anda akan diwisuda")
+
+#list
+y = [1, 2, 3, 4, 5]
+print(y[3])
+print(y[0]+y[1])
+
+
+makanan = ["soto", "mi ayam", "sate"]
+print(makanan[0])
+
+#mau tambah makanan bakso
+makanan.append("bakso")
+print(makanan[3])
+
+minuman = []
+harga = []
+
+minuman.append(input("masukan nama kontak: "))
+minuman.append(input("masukan nama kontak: "))
+minuman.append(input("masukan nama kontak: "))
+
+harga.append(input("masukan harga: "))
+harga.append(input("masukan harga: "))
+harga.append(input("masukan harga: "))
+
+print(minuman)
+print(harga)
+
+#mengganti minuman
+minuman = ["es teh", "es jeruk", "syrup",]
+
+minuman[0] = input("minuman pengganti: ")
+print(minuman)
+
+#Quiz
+c = 3
+a = c
+b = c + 1
+if b > a:
+    print("b is greater than a")
+elif c == b:
+    print("c is same like b")
+else :
+    print("other")
+
+
+    
+
+
