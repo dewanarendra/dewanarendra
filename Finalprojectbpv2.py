17a72bd1e47af8cf79226d4c7582fccb0920a964
unknown <dewanarendra115@gmail.com>
2021-12-25 22:57:13 +0700

    tambah file final project v2

--- Finalprojectbpv2.py
+++ Finalprojectbpv2.py
@@ -4,38 +4,22 @@ send email to multiple recipients
 '''
 
 import smtplib
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
 
 #definisi pengirim & penerima
 from_addr = "tespython11@gmail.com"
-to_addrs = ["John@example.com", "Mike@example.com"]
+to_addrs = ["johm@example.com","mike@example.com"]
+
 password = "pybasic11"
-
-f = open("receiver.txt","w")
-f.write = (to_addrs)
-f.close()
-
-#main
-msg = MIMEMultipart()
-msg["From"]    = from_addr
-msg["To"]      = to_addrs
-msg["Subject"] = "Final Project"
-
-body = '''\Welcome... 
-Final project Basic py batch 11 sent...
-.
-.
-.
-this is just a simple text to send
-'''
-msg.attach(MIMEText(body))
 
 server = smtplib.SMTP('smtp.gmail.com', 587)
 server.starttls()
 server.login(from_addr, password)
 
-text = msg.as_string() 
-server.sendmail(from_addr, to_addrs, text)
+msg = "Sending email using python"
+subject = "Final project"
+body = "Subject : {}\n\n{}".format(subject,msg)
+
+
+server.sendmail(from_addr,to_addrs,body)
 
 server.quit()
